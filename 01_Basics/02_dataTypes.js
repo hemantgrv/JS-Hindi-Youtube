f4fa9e44773db2bca2f334eea4cfb88bdb7ecabd
Hemant Gurav <123168636+hemantgrv@users.noreply.github.com>
2023-07-14 04:05:46 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict" // treat all JS code as newer version
+
+// alert( 3 + 3 ); we are using nodejs, not browser
+
+console.log(3 + 3);
+// code readability should be high
+
+console.log("Hemant");
+
+let name = "hemant" // string dataType
+let age = 20 // number dataType
+let isLoggedIn = true // boolean dataType
+
+
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean => true / false
+null => standalone value
+undefined => value is not defined
+symbol => unique
+
+
+object
+
+*/
+
+
+console.log(typeof null); // object 
+console.log(typeof undefined); // undefined
